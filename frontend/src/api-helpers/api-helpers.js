60f08d6a947a125b8cc3d12e9b09789c0a60b590
Amish Bajracharya <amishbajracharya63@gmail.com>
2024-07-07 15:56:26 +0545

    api helpers

--- frontend/src/api-helpers/api-helpers.js
+++ frontend/src/api-helpers/api-helpers.js
@@ -53,3 +53,99 @@ export const getMovieDetails = async (id) => {
   const resData = await res.data;
   return resData;
 };
+
+export const newBooking = async (data) => {
+  const res = await axios
+    .post("/booking", {
+      movie: data.movie,
+      seatNumber: data.seatNumber,
+      date: data.date,
+      user: localStorage.getItem("UserID"),
+    })
+    .catch((err) => console.log(err));
+
+  if (res.status !== 201) {
+    return console.log("Unexpected Error");
+  }
+  const resData = await res.data;
+  return resData;
+};
+
+export const getUserBooking = async () => {
+  const id = localStorage.getItem("UserID");
+  const res = await axios
+    .get(`/user/bookings/${id}`)
+    .catch((err) => console.log(err));
+
+  if (res.status !== 200) {
+    return console.log("Unexpected Error");
+  }
+  const resData = await res.data;
+  return resData;
+};
+
+export const deleteBooking = async (id) => {
+  const res = await axios
+    .delete(`/booking/${id}`)
+    .catch((err) => console.log(err));
+
+  if (res.status !== 200) {
+    return console.log("Unepxected Error");
+  }
+
+  const resData = await res.data;
+  return resData;
+};
+
+export const getUserDetails = async () => {
+  const id = localStorage.getItem("UserID");
+  const res = await axios.get(`/user/${id}`).catch((err) => console.log(err));
+  if (res.status !== 200) {
+    return console.log("Unexpected Error");
+  }
+  const resData = await res.data;
+  return resData;
+};
+
+export const addMovie = async (data) => {
+  const res = await axios
+    .post(
+      "/movie",
+      {
+        title: data.title,
+        description: data.description,
+        releaseDate: data.releaseDate,
+        posterUrl: data.posterUrl,
+        fetaured: data.fetaured,
+        actors: data.actors,
+        admin: localStorage.getItem("adminID"),
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      }
+    )
+    .catch((err) => console.log(err));
+
+  if (res.status !== 201) {
+    return console.log("Unexpected Error Occurred");
+  }
+
+  const resData = await res.data;
+  return resData;
+};
+
+export const getAdminById = async () => {
+  const adminId = localStorage.getItem("adminID");
+  const res = await axios
+    .get(`/admin/${adminId}`)
+    .catch((err) => console.log(err));
+
+  if (res.status !== 200) {
+    return console.log("Unexpected Error Occurred");
+  }
+
+  const resData = await res.data;
+  return resData;
+};
