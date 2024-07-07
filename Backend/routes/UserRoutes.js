7f44cb985d8edf9625e410f435746ae921dc8dcc
Amish Bajracharya <amishbajracharya63@gmail.com>
2024-07-07 21:52:34 +0545

    feat: getUserById

--- Backend/routes/UserRoutes.js
+++ Backend/routes/UserRoutes.js
@@ -1,13 +1,22 @@
 import express from "express";
-import { deleteUser, getAllBookings, getAllUsers, login, signup, updateUser } from "../controllers/UserControllers.js";
+import {
+  deleteUser,
+  getAllBookings,
+  getAllUsers,
+  login,
+  signup,
+  updateUser,
+  getUserById,
+} from "../controllers/UserControllers.js";
 
 const UserRouter = express.Router();
 
-UserRouter.get("/",getAllUsers);
-UserRouter.post("/signup",signup);
-UserRouter.put("/:id",updateUser);
-UserRouter.delete("/:id",deleteUser);
-UserRouter.post("/login",login);
-UserRouter.get("/bookings/:id",getAllBookings);
+UserRouter.get("/", getAllUsers);
+UserRouter.get("/:id", getUserById);
+UserRouter.post("/signup", signup);
+UserRouter.put("/:id", updateUser);
+UserRouter.delete("/:id", deleteUser);
+UserRouter.post("/login", login);
+UserRouter.get("/bookings/:id", getAllBookings);
 
 export default UserRouter;
