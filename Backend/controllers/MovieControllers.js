c7e0012ea0348e0dad0e20f3df98283b1d26a9a2
Amish Bajracharya <amishbajracharya63@gmail.com>
2024-07-07 21:15:43 +0545

    added ticketPrice

--- Backend/controllers/MovieControllers.js
+++ Backend/controllers/MovieControllers.js
@@ -4,90 +4,104 @@ import mongoose from "mongoose";
 import Admin from "../models/Admin.js";
 
 export const addMovie = async (req, res, next) => {
-    const extractedToken = req.headers.authorization.split(" ")[1];
-    if (!extractedToken || extractedToken.trim() === "") {
-        return res.status(404).json({ message: "Token Not found" });
-    }
+  const extractedToken = req.headers.authorization.split(" ")[1];
+  if (!extractedToken || extractedToken.trim() === "") {
+    return res.status(404).json({ message: "Token Not found" });
+  }
 
-    let adminId;
-    // Verify token
-    try {
-        const decrypted = Jwt.verify(extractedToken, process.env.SECRET_KEY);
-        adminId = decrypted.id;
-    } catch (err) {
-        return res.status(400).json({ message: `${err.message}` });
-    }
+  let adminId;
+  // Verify token
+  try {
+    const decrypted = Jwt.verify(extractedToken, process.env.SECRET_KEY);
+    adminId = decrypted.id;
+  } catch (err) {
+    return res.status(400).json({ message: `${err.message}` });
+  }
 
-    // Create new movie
-    const { title, cast, description, duration, releaseDate, posterUrl, trailerUrl } = req.body;
-    if (!title.trim() || !cast.trim() || !description.trim() || !duration.trim() || !releaseDate.trim() || !posterUrl.trim()) {
-        return res.status(402).json({ message: "Invalid Inputs" });
-    }
-    let existingMovie
-    try{
-        existingMovie = await Movie.findOne({title});
-    }
-    catch(err){
-        return console.log(err);
-    }
-    if(existingMovie){
-        return res.status(400).json({message:"Movie Already Added !"});
-    }
+  // Create new movie
+  const {
+    title,
+    cast,
+    description,
+    duration,
+    releaseDate,
+    posterUrl,
+    trailerUrl,
+    ticketPrice,
+  } = req.body;
+  if (
+    !title.trim() ||
+    !cast.trim() ||
+    !description.trim() ||
+    !duration.trim() ||
+    !releaseDate.trim() ||
+    !posterUrl.trim() ||
+    !ticketPrice.trim()
+  ) {
+    return res.status(402).json({ message: "Invalid Inputs" });
+  }
+  let existingMovie;
+  try {
+    existingMovie = await Movie.findOne({ title });
+  } catch (err) {
+    return console.log(err);
+  }
+  if (existingMovie) {
+    return res.status(400).json({ message: "Movie Already Added !" });
+  }
 
-    try {
-        const movie = new Movie({
-            title,
-            cast,
-            description,
-            releaseDate,
-            duration,
-            posterUrl,
-            trailerUrl,
-            admin: adminId,
-        });
+  try {
+    const movie = new Movie({
+      title,
+      cast,
+      description,
+      releaseDate,
+      duration,
+      posterUrl,
+      trailerUrl,
+      admin: adminId,
+      ticketPrice,
+    });
 
-        const session = await mongoose.startSession();
-        const adminUser = await Admin.findById(adminId);
-        session.startTransaction();
-        await movie.save({session});
-        adminUser.addedMovies.push(movie);
-        await adminUser.save({session});
-        await session.commitTransaction();
-        return res.status(200).json({ message: "Movie added successfully" , movie});
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: "Request failed" });
-    }
+    const session = await mongoose.startSession();
+    const adminUser = await Admin.findById(adminId);
+    session.startTransaction();
+    await movie.save({ session });
+    adminUser.addedMovies.push(movie);
+    await adminUser.save({ session });
+    await session.commitTransaction();
+    return res.status(200).json({ message: "Movie added successfully", movie });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Request failed" });
+  }
 };
 
-export const getAllMovies = async(req, res, next)=>{
-    let movies;
+export const getAllMovies = async (req, res, next) => {
+  let movies;
 
-    try {
-        movies = await Movie.find();
-        
-    } catch (err) {
-        return console.log(err);
-        
-    }
-if(!movies){
-    return res.status(500).json({message:"Request Failed"})
-}
-return res.status(200).json({ movies });
-
+  try {
+    movies = await Movie.find();
+  } catch (err) {
+    return console.log(err);
+  }
+  if (!movies) {
+    return res.status(500).json({ message: "Request Failed" });
+  }
+  return res.status(200).json({ movies });
 };
 
-export const getMoviesById = async (req, res, next)=>{
-    let id, movie;
-    id = req.params.id;
-    try {
-        movie = await Movie.findById(id);
-    } catch (err) {
-        return console.log(err);   
-    }
-    if(!id){
-        return res.status(500).json({message:"Request Failed"});
-    }
+export const getMoviesById = async (req, res, next) => {
+  let id, movie;
+  id = req.params.id;
+  try {
+    movie = await Movie.findById(id);
+  } catch (err) {
+    return console.log(err);
+  }
+  if (!id) {
+    return res.status(500).json({ message: "Request Failed" });
+  }
 
-    return res.status(200).json({ movie });
-}
+  return res.status(200).json({ movie });
+};
--- Backend/models/Movies.js
+++ Backend/models/Movies.js
@@ -1,46 +1,52 @@
 import mongoose from "mongoose";
 
-const movieSchema = new mongoose.Schema ({
-    title: {
-        type: String,
-        required: true,
+const movieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  cast: [
+    {
+      type: String,
+      required: true,
     },
-    cast:[{
-        type:String,
-        required: true,
-    }],
-    description: {
-        type: String,
-        required: true,
+  ],
+  description: {
+    type: String,
+    required: true,
+  },
+  releaseDate: {
+    type: Date,
+    required: true,
+  },
+  duration: {
+    type: String,
+    required: true,
+  },
+  posterUrl: {
+    type: String,
+    required: true,
+  },
+  trailerUrl: {
+    type: String,
+    required: true,
+  },
+  featured: {
+    type: Boolean,
+  },
+  bookings: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Bookings",
     },
-    releaseDate:{
-        type: Date,
-        required:true,
-    },
-    duration: {
-        type: String,
-        required: true,
-    },
-    posterUrl:{
-        type:String,
-        required:true,
-    },
-    trailerUrl:{
-        type:String,
-        required:true,
-
-    },
-    featured: {
-        type: Boolean,
-        
-    },
-    bookings:[{
-        type:mongoose.Types.ObjectId, ref:"Bookings",
-    }],
-    admin:{
-        type: mongoose.Types.ObjectId,
-        ref :"Admin",
-    }
-
+  ],
+  admin: {
+    type: mongoose.Types.ObjectId,
+    ref: "Admin",
+  },
+  ticketPrice: {
+    type: Number,
+    required: true,
+  },
 });
-export default mongoose.model('Movie', movieSchema);
+export default mongoose.model("Movie", movieSchema);
